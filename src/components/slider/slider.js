8400a643bff43e684ea24155a40578ce8345b7e4
Cleber Otaviano <cleberotaviano@live.com>
2023-09-24 15:50:18 -0300

    arco luminoso

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -9,6 +9,7 @@ import template from "./slider.html?raw";
 // };
 var xAxis = 0;
 const yAxis = 100;
+const widthArc = 140;
 
 export function sliderComponent() {
     return `${template}`;
@@ -25,30 +26,27 @@ function init() {
     updateSizeSlider();
     // moveSlider();
     moveSliderMobile();
-    slider.style.left = xAxis + 50 + 'px';
+    slider.style.left = xAxis + 'px';
 }
 
 function updateSizeSlider() {
     let sizePage = window.innerWidth;
     let path = document.getElementById('gradient-path');
-
     // let d = path.getAttribute('d');
     let dString = `
-        M0 10 L${sizePage} 10
         M0 100 L${xAxis + 20} 100
 
+        M${xAxis + 40} 80 A 50 50 0 0 1 ${xAxis + widthArc} 80
 
-        M100 80 A 50 50 0 0 1 200 80
+        M${xAxis + 20} 100
+        C${xAxis + 20}, 100
+        ${xAxis + 40} 100
+        ${xAxis + 40} 80
 
-        M80 100
-        C80, 100
-        100 100
-        100 80
-
-        M200 80
-        C200, 80,
-        200 100
-        220 100
+        M${xAxis + widthArc} 80
+        C${xAxis + widthArc}, 80,
+        ${xAxis + widthArc} 100
+        ${xAxis + widthArc + 20} 100
 
         M${(xAxis + 160) < sizePage ? (xAxis + 160) : sizePage } 100
         ${sizePage} 100
@@ -63,15 +61,16 @@ function moveSliderMobile() {
     let debuggerElement = document.getElementById('debugger');
 
     dragArea.ontouchmove = (event) => {
+        event.preventDefault();
         var touch = event.targetTouches[0];
-        let axis = touch.pageX - 20;
+        let axis = touch.pageX - 90;
 
-        if (axis >= 0 && axis <= (window.innerWidth - 50)) {
+        if (axis >= 0 && axis <= (window.innerWidth - widthArc - 40)) {
             xAxis = axis;
             slider.style.left = axis + 'px';
             debuggerElement.innerText = touch.pageX - 20
         }
-        event.preventDefault();
+
         updateDebuggers();
         updateSizeSlider();
     }
